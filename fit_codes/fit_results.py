357df28cd61549e055bdc34be46975991efd8018
Anibal <anibalvarela134@gmail.com>
2023-08-14 16:52:51 -0700

    functions that read results of fits

---
+++ fit_codes/fit_results.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from filter_curves import filtros
+
+def cov_matrix(name_file):
+    data = np.load(name_file,allow_pickle=True)
+    covariance_matrix = data.item()['covariance_matrix']
+    return covariance_matrix[0:8,0:8]
+
+def chi_dof(path_model, path_file):
+    data = np.load(path_file,allow_pickle=True)
+    curvas, params = filtros(path_model)
+    if "Roman" in path_file:
+        npoints = len(curvas['w'])
+        chi2 = data.item()['chi2']
+        best_model = data.item()['best_model']
+        dof = npoints-len(best_model)
+        return chi2/dof
+    else:
+        npoints = len(curvas['w'])+len(curvas['u'])+len(curvas['g'])+len(curvas['r'])+len(curvas['i'])+len(curvas['z'])+len(curvas['y'])
+        chi2 = data.item()['chi2']
+        best_model = data.item()['best_model']
+        dof = npoints-len(best_model)
+        return chi2/dof
+
+def errors(name_file):
+    data = np.load(name_file,allow_pickle=True)
+    covariance_matrix = data.item()['covariance_matrix']
+    errors = np.sqrt(abs(np.diagonal(covariance_matrix)))
+    return errors
+
+def best_model(name_file):
+    data = np.load(name_file,allow_pickle=True)
+    return data.item()['best_model'][0:8]
+
