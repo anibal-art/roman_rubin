cce70df8413cceec137477e09faa909aae39e7cd
Anibal <anibalvarela134@gmail.com>
2023-08-15 13:36:42 -0700

    new implementation of fit_rubin_roman

--- fit_codes/fit_application.py
+++ fit_codes/fit_application.py
@@ -14,7 +14,7 @@ from pyLIMA.outputs import file_outputs
 import pandas as pd
 import pygtc
 from filter_curves import filtros
-from fit_events import fit_rubin_roman, fit_roman
+from fit_events import fit_rubin_roman
 
 path_ephemerides = '/home/anibal/files_db/james_webb.txt'
 path_save = '/home/anibal/roman_rubin/event_7_analisys/DE/'
@@ -31,9 +31,10 @@ def fit_light_curve(file_name,algo):
     light_curve,PARAMS = filtros(file_name)
     if not light_curve == 0:
         array_w149, array_u, array_g, array_r, array_i, array_z, array_y = light_curve['w'],light_curve['u'],light_curve['g'],light_curve['r'],light_curve['i'],light_curve['z'],light_curve['y']
-
+	
         fit_2, e, tel_list = fit_rubin_roman(PARAMS['Source'],PARAMS,path_save,path_ephemerides,algo, array_w149, array_u, array_g, array_r, array_i, array_z, array_y)
-        fit_2, e, tel_list = fit_roman(PARAMS['Source'],PARAMS,path_save,path_ephemerides,algo, array_w149)
+        fit_2, e, tel_list = fit_rubin_roman(PARAMS['Source'],PARAMS,path_save,path_ephemerides,algo, array_w149, [], [], [], [], [], [])
+        #fit_roman(PARAMS['Source'],PARAMS,path_save,path_ephemerides,algo, array_w149)
         return fit_2, e, tel_list
     else:
         print('This event is not selected and can´t be fitted.')
