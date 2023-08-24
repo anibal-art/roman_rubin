7cf612303eee89a4a0d8a7abaa4b12bc2c9e218a
Anibal <anibalvarela134@gmail.com>
2023-08-24 01:59:27 -0700

    restore code

--- fit_codes/fit_set.py
+++ fit_codes/fit_set.py
@@ -16,14 +16,15 @@ def wait_timeout(proc, seconds):
         if time.time() >= end:
             proc.kill()
             raise RuntimeError("Process timed out")
-
         time.sleep(interval)
 
-
 file_events = os.listdir('/home/anibal/files_db/filtered_curves/')
+path_ephemerides = '/home/anibal/files_db/james_webb.txt'
+path_save = '/home/anibal/roman_rubin/test_badm1/'
+path_model = '/home/anibal/files_db/filtered_curves/' #PATH OF THE DIRECTORY OF THE LIGHT CURVE THAT I WANT TO FIT
 
 def fitea():
-    for i in range(len(file_events)):
+    for i in range(10):
 
         print('---------------------------------------------------')
         print('------------ FIT EVENT', file_events[i], 'STARTING!! ----------')
@@ -31,7 +32,9 @@ def fitea():
 #         tinit = time()
 
         try:
-            process = subprocess.Popen(["python", "-c", f"from fit_application import fit_light_curve; fit_light_curve('/home/anibal/files_db/filtered_curves/{file_events[i]}', 'TRF')"])
+            process = subprocess.Popen([ "python", "-c",
+                                         f"from fit_application import fit_light_curve; fit_light_curve('/home/anibal/files_db/filtered_curves/{file_events[i]}',"
+                                         f" 'TRF','{path_ephemerides}', '{path_save}')"])
             wait_timeout(process, 15*60 )
         except RuntimeError as e:
             print("Process error:", str(e))
