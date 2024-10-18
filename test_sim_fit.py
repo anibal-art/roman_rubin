b3aead586827333d9246230e5106cf8812a3bc9c
anibal-art <anibalvarela134@gmail.com>
2024-10-18 12:44:30 -0300

    make the code run inside the git directory

--- test_sim_fit.py
+++ test_sim_fit.py
@@ -1,5 +1,9 @@
 from functions_roman_rubin import sim_fit
+import os
 import numpy as np
+import pandas as pd
+current_path = os.getcwd()
+print(current_path)
 # path_ephemerides = '/home/anibalvarela/ajustes/james_webb.txt'
 # path_dataslice = '/home/anibalvarela/opsims/baseline/dataSlice.npy'
 # path_storage = '/share/storage3/rubin/microlensing/romanrubin'
@@ -13,9 +17,9 @@ import numpy as np
 i=0
 model='USBL'
 algo='TRF'
-path_TRILEGAL_set= '/home/anibal/roman_rubin_paper/TRILEGAL/PB_planet_split_1.csv'
-path_to_save_model='/home/anibal/roman_rubin_paper/test_sim_fit/'
-path_to_save_fit= '/home/anibal/roman_rubin_paper/test_sim_fit/'
-path_ephemerides= '/home/anibal/files_db/james_webb.txt'
-path_dataslice = '/home/anibal/roman_rubin_paper/opsims/baseline/dataSlice.npy'
+path_TRILEGAL_set= current_path+'/TRILEGAL/PB_planet_split_1.csv'
+path_to_save_model= current_path+'/test_sim_fit/'
+path_to_save_fit= current_path+'/test_sim_fit/'
+path_ephemerides= current_path+'/ephemerides/james_webb.txt'
+path_dataslice = current_path+'/opsims/baseline/dataSlice.npy'
 sim_fit(i,model,algo,path_TRILEGAL_set,path_to_save_model,path_to_save_fit,path_ephemerides,path_dataslice)
