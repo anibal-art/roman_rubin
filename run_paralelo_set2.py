255f20d9f19c2d7083e3ee61eb26f831fac55a37
Anibal Varela <anibalvarela@che.cbpf.br>
2024-10-22 00:07:39 -0300

    files to paralelize

--- run_paralelo_set2.py
+++ run_paralelo_set2.py
@@ -2,7 +2,7 @@ from sim_fit_paralelos import para_elisa
 path_ephemerides = '/home/anibalvarela/ajustes/james_webb.txt'
 path_dataslice = '/home/anibalvarela/opsims/baseline/dataSlice.npy'
 path_storage = '/share/storage3/rubin/microlensing/romanrubin'
-j=1
+j=2
 path_TRILEGAL_set =f'/home/anibalvarela/TRILEGAL/BH_split_{j}.csv'#PB_planet_split_{j}.csv'# "/home/anibal/results_roman_rubin/PB_planet_split_1.csv"FFP_uni_split_3.csv
 path_to_save_model = path_storage+f"/BH/set_sim{j}/"
 path_to_save_fit = path_storage+f"/BH/set_fit{j}/"
