edb4ab512593e2a477c33cf221847e87213af0ca
anibal-art <anibalvarela134@gmail.com>
2024-10-23 17:57:09 -0300

    new file

---
+++ MCMC_fit.py
@@ -0,0 +1,23 @@
+import os, sys
+import numpy as np
+import matplotlib.pyplot as plt
+from pyLIMA.outputs import pyLIMA_plots
+from cycler import cycler
+
+sys.path.append(os.path.dirname(os.getcwd()))
+from functions_roman_rubin import sim_fit
+from functions_roman_rubin import model_rubin_roman
+from functions_roman_rubin import read_data
+
+current_path = os.path.dirname(os.getcwd())
+# print(current_path)
+i=18
+model='USBL'
+algo='MCMC'
+path_TRILEGAL_set= current_path+'/TRILEGAL/PB_planet_split_1.csv'
+path_to_save_model= current_path+'/MCMC_fit/'
+path_to_save_fit= current_path+'/MCMC_fit/'
+path_ephemerides= current_path+'/ephemerides/Gaia.txt'
+path_dataslice = current_path+'/opsims/baseline/dataSlice.npy'
+
+sim_fit(i,model,algo,path_TRILEGAL_set,path_to_save_model,path_to_save_fit,path_ephemerides,path_dataslice)
