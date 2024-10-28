7845fe41e0f33d016b0887ed86c7aa72f2754ef7
anibal-art <anibalvarela134@gmail.com>
2024-10-28 15:13:01 -0300

    add uncertainty propgation with montecarlo in piE to analisis

--- montecarlo_propagation.py
+++ montecarlo_propagation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import pygtc
-import matplotlib.pyplot as plt
+
 params_str = ["t0","u0", "te","rho","s","q","alpha","piEN", "piEE" ]
 
 path = "/share/storage3/rubin/microlensing/romanrubin/PB/set_fit1/"
@@ -11,12 +11,10 @@ rand_index = np.random.randint(0,len(files_fit))
 data_fit = np.load(path+files_fit[rand_index], allow_pickle=True).item()
 mean_for_sampling = data_fit["best_model"][0:9]
 cov_for_sampling = data_fit['covariance_matrix'][0:9,0:9]
+piE = np.sqrt(mean_for_sampling[7]**2+mean_for_sampling[8]**2)
 
 n_samples_plot = 30000
 samples = np.random.multivariate_normal(mean_for_sampling, cov_for_sampling, n_samples_plot)
-
-piE = np.sqrt(mean_for_sampling[7]**2+mean_for_sampling[8]**2)
-
 piEN_dist = np.array(samples)[:,7]
 piEE_dist = np.array(samples)[:,8]
 piE_dist = np.sqrt(piEN_dist**2+piEE_dist**2)
