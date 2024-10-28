19363861ddf529ed8696a342f3a7607df26a1850
anibal-art <anibalvarela134@gmail.com>
2024-10-28 12:49:18 -0300

    comparison MC and formula

---
+++ montecarlo_propagation.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import pygtc
+import matplotlib.pyplot as plt
+params_str = ["t0","u0", "te","rho","s","q","alpha","piEN", "piEE" ]
+
+path = "/home/anibal/roman_rubin/test_sim_fit/Event_RR_18_TRF.npy"
+
+data_fit = np.load(path, allow_pickle=True).item()
+mean_for_sampling = data_fit["best_model"][0:9]
+cov_for_sampling = data_fit['covariance_matrix'][0:9,0:9]
+
+n_samples_plot = 30000
+samples = np.random.multivariate_normal(mean_for_sampling, cov_for_sampling, n_samples_plot)
+
+piE = np.sqrt(mean_for_sampling[7]**2+mean_for_sampling[8]**2)
+
+piEN_dist = np.array(samples)[:,7]
+piEE_dist = np.array(samples)[:,8]
+piE_dist = np.sqrt(piEN_dist**2+piEE_dist**2)
+
+
+
+errors = np.sqrt(np.diag(data_fit['covariance_matrix'][0:9,0:9]))
+piEE_err = errors[7]
+piEN_err = errors[8]
+cov_piEE_piEN = data_fit['covariance_matrix'][8:9,8:9]
+
+piEE = mean_for_sampling[7]
+piEN = mean_for_sampling[8]
+
+piE_err = (1/piE)*np.sqrt((piEN_err*piEN)**2+(piEE_err*piEE)**2+2*piEE*piEN*cov_piEE_piEN)
+print("The value estimated is ", piE)
+print("The uncertainty using MC is ", np.std(piE_dist))
+print("The uncertainty using formulae for error propagation is ", piE_err)
