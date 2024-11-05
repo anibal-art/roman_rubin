19a4078a91098a1f61ec0359efe5aec1c9665821
anibal-art <anibalvarela134@gmail.com>
2024-11-05 19:22:29 -0300

    class modification

--- class_analysis.py
+++ class_analysis.py
@@ -44,13 +44,18 @@ class Analysis_Event:
             fit_error= np.sqrt(np.diag(data['covariance_matrix']))[0:len(self.labels_params())]
         return fit_error
     
+    def data_fit(self):
+        data_rr = np.load(self.path_fit_rr, allow_pickle=True).item()
+        data_roman = np.load(self.path_fit_roman, allow_pickle=True).item()
+        return data_rr, data_roman
+    
     def fit_true(self):
 
         fit_rr = {}
         fit_roman = {}
         
-        data_rr = np.load(self.path_fit_rr, allow_pickle=True).item()
-        data_roman = np.load(self.path_fit_roman, allow_pickle=True).item()
+        data_rr, data_roman = self.data_fit()
+        
         fit_error_rr = self.error_pyLIMA(data_rr)
         fit_error_roman = self.error_pyLIMA(data_roman)
 
@@ -82,41 +87,26 @@ class Analysis_Event:
             return info_dataset, pyLIMA_parameters, bands
     
     
-    # def chichi(self):
-    #     '''
-    #     name_file(str):This function receives as input the name of the file
-    #     example: /home/user/model/set_sim1/Event_RR_42_trf.npy.
-    #     '''
-    #     # print(name_file[name_file.index('Event_')-2])
-    #     nset = int(name_file[name_file.index('Event_') - 2:name_file.index('Event_') - 1])
-    
-    #     # print(nset)
-    #     name_set = 'set_sim' + str(nset)
-    #     directory_simset = name_file[0:name_file.index('set')] + name_set + '/'
-    #     # print()
-    #     nevent = re.sub(r'\D', '', name_file[name_file.index('Event_'):-1])
-    
-    #     model_file = directory_simset + 'Event_' + str(nevent) + '.h5'
-    #     info_dataset, model_params, curves = self.read_data(name_file)
-    #     # curves,model_params = read_curves(model_file)
-    #     name_file_rr = name_file[0:name_file.index('set')] + f"/set_fit{nset}/Event_RR_{nevent}_TRF.npy"
-    #     name_file_roman = name_file[0:name_file.index('set')] + f"/set_fit{nset}/Event_Roman_{nevent}_TRF.npy"
-    #     data_rr = np.load(name_file_rr, allow_pickle=True)
-    #     data_roman = np.load(name_file_roman, allow_pickle=True)
-    #     # print(data_rr)
-    #     try:
-    #         chi_rr = data_rr.item()["chi2"]
-    #         chi_roman = data_roman.item()["chi2"]
-    
-    #         dof_rr = sum([len(curves[key]) for key in curves]) - len(
-    #             [len(curves[key]) for key in curves if not len(curves[key]) == 0]) * 2 - 9
-    #         dof_roman = len(curves['W149']) - 2 - 9
-    #         # print(model_params)
-    #         # print(chi_roman/dof)
-    #         return chi_rr, chi_roman, dof_rr, dof_roman
-    #     except:
-    #         return 0, 0,0,0
-    
+    def chichi(self):
+        '''
+        name_file(str):This function receives as input the name of the file
+        example: /home/user/model/set_sim1/Event_RR_42_trf.npy.
+        '''
+
+        info_dataset, model_params, curves = self.read_data()
+        data_rr, data_roman = self.data_fit()
+
+        
+        chi_rr = data_rr["chi2"]
+        chi_roman = data_roman["chi2"]
+
+        dof_rr = sum([len(curves[key]) for key in curves]) - len(
+            [len(curves[key]) for key in curves if not len(curves[key]) == 0]) * 2 - len(self.labels_params())
+        dof_roman = len(curves['W149']) - 2 - len(self.labels_params())
+
+        return chi_rr, chi_roman, dof_rr, dof_roman
+
+
     def group_consecutive_numbers(numbers):
         '''
         Defino comienzo y finalizacion de temporadas de observacion de
@@ -144,30 +134,30 @@ class Analysis_Event:
         return (start1 <= end2 and end1 >= start2) or (start2 <= end1 and end2 >= start1)
     
     
-    def chichi_to_fits_files(self,path,fit_rr, fit_roman):
+    # def chichi_to_fits_files(self,path,fit_rr, fit_roman):
         
-        id_to_chi2_rr = {}
-        id_to_chi2_roman = {}
-        id_to_dof_rr = {}
-        id_to_dof_roman = {}
-        sets = [int(re.search(r'\d+', f).group()) for f in os.listdir(path) if 'set_sim' in f]
+    #     id_to_chi2_rr = {}
+    #     id_to_chi2_roman = {}
+    #     id_to_dof_rr = {}
+    #     id_to_dof_roman = {}
+    #     sets = [int(re.search(r'\d+', f).group()) for f in os.listdir(path) if 'set_sim' in f]
         
-        for i in tqdm(sets):
-            common_elements_list = self.event_fits(path+f"set_fit{i}/")
-            if not len(common_elements_list)==0:
-                for j in range(len(common_elements_list)):
-                    name_file = f"Event_{common_elements_list[j]}.h5"
-                    chi2rr, chi2roman,dof_rr,dof_roman = self.chichi(path+f"set_sim{i}/"+name_file)
-                    # print(chi2rr, chi2roman)
-                    id_to_chi2_rr[int(common_elements_list[j]+i*5000)] =chi2rr
-                    id_to_chi2_roman[int(common_elements_list[j]+i*5000)] =chi2roman
-                    id_to_dof_rr[int(common_elements_list[j]+i*5000)] = dof_rr
-                    id_to_dof_roman[int(common_elements_list[j]+i*5000)] = dof_roman
-        fit_rr['chi2'] = fit_rr['Source'].map(id_to_chi2_rr)
-        fit_roman['chi2'] = fit_roman['Source'].map(id_to_chi2_roman)
-        fit_roman['dof']= fit_roman['Source'].map(id_to_dof_roman)
-        fit_rr['dof']= fit_rr['Source'].map(id_to_dof_rr)
-        return fit_rr, fit_roman
+    #     for i in tqdm(sets):
+    #         common_elements_list = self.event_fits(path+f"set_fit{i}/")
+    #         if not len(common_elements_list)==0:
+    #             for j in range(len(common_elements_list)):
+    #                 name_file = f"Event_{common_elements_list[j]}.h5"
+    #                 chi2rr, chi2roman,dof_rr,dof_roman = self.chichi(path+f"set_sim{i}/"+name_file)
+    #                 # print(chi2rr, chi2roman)
+    #                 id_to_chi2_rr[int(common_elements_list[j]+i*5000)] =chi2rr
+    #                 id_to_chi2_roman[int(common_elements_list[j]+i*5000)] =chi2roman
+    #                 id_to_dof_rr[int(common_elements_list[j]+i*5000)] = dof_rr
+    #                 id_to_dof_roman[int(common_elements_list[j]+i*5000)] = dof_roman
+    #     fit_rr['chi2'] = fit_rr['Source'].map(id_to_chi2_rr)
+    #     fit_roman['chi2'] = fit_roman['Source'].map(id_to_chi2_roman)
+    #     fit_roman['dof']= fit_roman['Source'].map(id_to_dof_roman)
+    #     fit_rr['dof']= fit_rr['Source'].map(id_to_dof_rr)
+    #     return fit_rr, fit_roman
     
     
     def montecarlo_propagation_piE(best_model, covariance_matrix, indx_piE):
@@ -267,7 +257,7 @@ class Analysis_Event:
     
     
     
-    def piE_cov_terms(path,fit_rr,fit_roman, labels_params):
+    def piE_propagation(self):
         """
         Parameters
         ----------
@@ -288,12 +278,7 @@ class Analysis_Event:
             Add a column with the mass obtained from MonteCarlo propagation.
             Add a column with the mass obtained from propagation formulae.
         """
-        mass_true = {}
-        mass_MC_rho_rr = {}
-        mass_MC_rho_rom = {}
-        mass_MC_te_rr = {}
-        mass_MC_te_rom = {}
-    
+ 
         cov_piEE_piEN = {}
         cov_piEE_piEN_rom = {}
     
@@ -301,86 +286,56 @@ class Analysis_Event:
         piE_MC_roman = {}
     
     
-        current_directory = os.getcwd()
-        if len(labels_params)==len(['t0','u0','te','rho',"s","q","alpha",'piEN','piEE']):
-            indx_tE_rho = [2,3]
+        if len(self.labels_params)==len(['t0','u0','te','rho',"s","q","alpha",'piEN','piEE']):
             indx_piE = [7, 8]
-            path_TRILEGAL = lambda nset: current_directory+f'/TRILEGAL/PB_planet_split_{nset}.csv'
-        elif len(labels_params)==len(['t0','u0','te','rho','piEN','piEE']):
             indx_tE_rho = [2,3]
+        elif len(self.labels_params)==len(['t0','u0','te','rho','piEN','piEE']):
             indx_piE = [4, 5]
-            path_TRILEGAL = lambda nset: current_directory+f'/TRILEGAL/FFP_uni_split_{nset}.csv'
-        elif len(labels_params)==len(['t0','u0','te','piEN','piEE']):
-            indx_tE_rho = [2]
+            indx_tE_rho = [2,3]
+        elif len(self.labels_params)==len(['t0','u0','te','piEN','piEE']):
             indx_piE = [3, 4]
-            path_TRILEGAL = lambda nset: current_directory+f'/TRILEGAL/BH_split_{nset}.csv'
-            
-        for i in tqdm(range(len(fit_rr))):
-            nsource = fit_rr["Source"].iloc[i]
-            nset = int(nsource / 5000)
-            nevent = nsource - nset * 5000
-            data = np.load(path + f"set_fit{nset}/Event_RR_{nevent}_TRF.npy", allow_pickle=True)
-            data_rom = np.load(path + f"set_fit{nset}/Event_Roman_{nevent}_TRF.npy", allow_pickle=True)
-            
-            path_TRILEGAL_set= path_TRILEGAL(nset)
-            TRILEGAL_data = pd.read_csv(path_TRILEGAL_set)
-            
-            
-            mu_rel = TRILEGAL_data["mu_rel"].iloc[int(nevent)]
-            print(mu_rel)
-            Rstar = TRILEGAL_data["radius"].iloc[int(nevent)]
-            print(Rstar)
-            DS = TRILEGAL_data["D_S"].iloc[int(nevent)]
-            print(DS)
-            thetas = np.arctan(Rstar/DS)*u.rad.to('mas')
-            print(thetas)
-             # rho_true = TRILEGAL_data["rho"]
-            c = const.c
-            G = const.G
-            # yr2day = 365.25
-            k = 4 * G / (c ** 2)
-            # aconv = (180 * 60 * 60 * 1000) / np.pi
-            # piE_true = np.sqrt(TRILEGAL_data["piEE"]**2+TRILEGAL_data["piEN"]**2)
-            # thetaE_true = TRILEGAL_data["te"]*TRILEGAL_data["mu_rel"]
-            # m_true = thetaE_true/(k*piE_true)
-            
-            best_model = data.item()['best_model']
-            covariance_matrix = data.item()['covariance_matrix']
-            MC_tE, MC_rho, MC_piE = MC_tE_rho_piE(best_model, covariance_matrix, indx_tE_rho, indx_piE)
-            thetaE_tE = MC_tE*mu_rel
-            thetaE_rho = thetas/MC_rho
-            mass_rr_tE = thetaE_tE/(k*MC_piE)
-            mass_rr_rho = thetaE_rho/(k*MC_piE)
-    
-            best_model_rom = data_rom.item()['best_model']
-            covariance_matrix_rom = data_rom.item()['covariance_matrix']
-            MC_tE_rom, MC_rho_rom, MC_piE_rom = MC_tE_rho_piE(best_model_rom, covariance_matrix_rom, indx_tE_rho,indx_piE)
-            thetaE_tE_rom = MC_tE_rom*mu_rel
-            thetaE_rho_rom = thetas/MC_rho_rom
-            mass_rom_tE = thetaE_tE_rom/(k*MC_piE_rom)
-            mass_rom_rho = thetaE_rho_rom/(k*MC_piE_rom)
-    
-            mass_MC_rho_rr[nsource] = np.std(mass_rr_rho)
-            mass_MC_rho_rom[nsource] = np.std(mass_rom_rho)
-    
-            mass_MC_te_rr[nsource] = np.std(mass_rr_tE)
-            mass_MC_te_rom[nsource] = np.std(mass_rom_tE)
-    
-            piE_MC_rr[nsource] = np.std(MC_piE)
-            piE_MC_roman[nsource] = np.std(MC_piE_rom)
-    
+            indx_tE_rho = [2,3]
+
+        data_rr, data_roman = self.data_fit()
+        
+        MC_tE, MC_rho, MC_piE = self.MC_tE_rho_piE(data_rr["best_model"],
+                                              data_rr["covariance_matrix"],
+                                              indx_tE_rho, indx_piE)
+        # thetaE_tE = MC_tE*mu_rel
+        # thetaE_rho = thetas/MC_rho
+        # mass_rr_tE = thetaE_tE/(k*MC_piE)
+        # mass_rr_rho = thetaE_rho/(k*MC_piE)
+
+
+        MC_tE_rom, MC_rho_rom, MC_piE_rom = self.MC_tE_rho_piE(data_roman["best_model"],
+                                              data_roman["covariance_matrix"],
+                                              indx_tE_rho, indx_piE)
+        # thetaE_tE_rom = MC_tE_rom*mu_rel
+        # thetaE_rho_rom = thetas/MC_rho_rom
+        # mass_rom_tE = thetaE_tE_rom/(k*MC_piE_rom)
+        # mass_rom_rho = thetaE_rho_rom/(k*MC_piE_rom)
+
+        # mass_MC_rho_rr[nsource] = np.std(mass_rr_rho)
+        # mass_MC_rho_rom[nsource] = np.std(mass_rom_rho)
+
+        # mass_MC_te_rr[nsource] = np.std(mass_rr_tE)
+        # mass_MC_te_rom[nsource] = np.std(mass_rom_tE)
+
+        piE_MC_rr= np.std(MC_piE)
+        piE_MC_roman = np.std(MC_piE_rom)
+
     
         #mapeo a los df fit_rr y fit_roman
-        fit_rr["cov_piEE_piEN"] = fit_rr['Source'].map(cov_piEE_piEN)
-        fit_roman["cov_piEE_piEN"] = fit_rr['Source'].map(cov_piEE_piEN_rom)
-        fit_rr['piE_err_MC'] = fit_rr['Source'].map(piE_MC_rr)
-        fit_roman['piE_err_MC'] = fit_roman['Source'].map(piE_MC_roman)
+        # fit_rr["cov_piEE_piEN"] = fit_rr['Source'].map(cov_piEE_piEN)
+        # fit_roman["cov_piEE_piEN"] = fit_rr['Source'].map(cov_piEE_piEN_rom)
+        # fit_rr['piE_err_MC'] = fit_rr['Source'].map(piE_MC_rr)
+        # fit_roman['piE_err_MC'] = fit_roman['Source'].map(piE_MC_roman)
     
-        fit_rr['mass_MC_te'] = fit_rr['Source'].map(mass_MC_te_rr)
-        fit_roman['mass_MC_te'] = fit_roman['Source'].map(mass_MC_te_rom)
+        # fit_rr['mass_MC_te'] = fit_rr['Source'].map(mass_MC_te_rr)
+        # fit_roman['mass_MC_te'] = fit_roman['Source'].map(mass_MC_te_rom)
     
-        fit_rr['mass_MC_rho'] = fit_rr['Source'].map(mass_MC_rho_rr)
-        fit_roman['mass_MC_rho'] = fit_roman['Source'].map(mass_MC_rho_rom)
+        # fit_rr['mass_MC_rho'] = fit_rr['Source'].map(mass_MC_rho_rr)
+        # fit_roman['mass_MC_rho'] = fit_roman['Source'].map(mass_MC_rho_rom)
     
         #opero solo con los df
         fit_rr['piE'] = np.sqrt(fit_rr['piEN'] ** 2 + fit_rr['piEE'] ** 2)
@@ -506,3 +461,4 @@ true, fit_rr, fit_roman = Event.fit_true()
 
 print(fit_rr)
 print(Event.read_data())
+print(Event.chichi())
