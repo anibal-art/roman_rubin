badb2eda26d1f5ff0e9e126cfb408bd2103abb8b
anibal <anibalvarela134@gmail.com>
2025-02-14 14:41:13 -0300

    update

--- run_class_analysis.py
+++ run_class_analysis.py
@@ -7,21 +7,20 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-# Here I set the model
+# Here I set the model path
 model = "USBL"
 path_run = '/share/storage3/rubin/microlensing/romanrubin/PB'
-
 
 script_dir = str(Path(__file__).parent)
 print(script_dir)
 path_dataslice = script_dir+'/opsims/baseline/dataSlice.npy'
 
-
 save_results = script_dir + "/all_results/"+model+"/"
 os.makedirs(save_results, exist_ok=True)
 
+# columnas de df
 cols_true = ['Source', 'Set'] + labels_params(model) + ['Category','mass']
-true_df = pd.DataFrame(columns=cols_true)
+
 
 cols_fit = ['Source', 'Set'] + labels_params(model)  + \
 [f+'_err' for f in labels_params(model) ]+\
@@ -29,8 +28,20 @@ cols_fit = ['Source', 'Set'] + labels_params(model)  + \
 ['mass_thetaE','mass_mu', 'mass_thetaS','err_mass_thetaE_NotMC', 
  'mass_err_thetaE' ,'mass_err_mu','mass_err_thetaS', 'chichi', 'dof']
 
-fit_rr_df = pd.DataFrame(columns=cols_fit)
-fit_roman_df = pd.DataFrame(columns=cols_fit)
+# filenames
+filename_true = save_results+'true.csv'
+filename_fit_rr = save_results+'fit_rr.csv'
+filename_fit_roman = save_results+'fit_roman.csv'
+
+if not os.path.exists(filename):
+    true_df = pd.DataFrame(columns=cols_true)
+    fit_rr_df = pd.DataFrame(columns=cols_fit)
+    fit_roman_df = pd.DataFrame(columns=cols_fit)
+
+    true_df.to_csv(filename_true, index=False)
+    fit_roman_df.to_csv(filename_fit_roman, index=False)
+    fit_rr_df.to_csv(filename_fit_rr, index=False)
+
 
 for SET in tqdm(range(1,5)):
     nset=SET
@@ -61,33 +72,33 @@ for SET in tqdm(range(1,5)):
         #err_piE_rr_MC , err_piE_roman_MC =  Event.MC_propagation_piE()
         #dict_mass = Event.mass_MC()
         
-        
-        new_row = {}
-        new_row['Source'] = nevent
-        new_row['Set'] = nset
+        new_data_true = pd.DataFrame(columns=cols_true)
+        #new_row = {}
+        new_data_true['Source'] = nevent
+        new_data_true['Set'] = nset
         for key in Event.labels_params():
-            new_row[key]=true[key]
+            new_data_true[key]=true[key]
         
-        new_row['Category'] = Event.categories_function()
-        new_row['mass'] = Event.mass_true()
-        true_df = pd.concat([true_df, pd.DataFrame([new_row])], ignore_index=True)
-        
+        new_data_true['Category'] = Event.categories_function()
+        new_data_true['mass'] = Event.mass_true()
+        #true_df = pd.concat([true_df, pd.DataFrame([new_row])], ignore_index=True)
+        new_data.to_csv(filename_true, mode="a", header=False, index=False)
         
         # df de Roman+Rubin
+        new_data_rr = pd.DataFrame(columns=cols_fit)
         
-        new_row = {}
-        new_row['Source'] = nevent
-        new_row['Set'] = nset
+        new_data_rr['Source'] = nevent
+        new_data_rr['Set'] = nset
         for key in Event.labels_params():
-            new_row[key]=fit_rr[key]
-            new_row[key+'_err']=fit_rr[key+'_err']
+            new_data_rr[key]=fit_rr[key]
+            new_data_rr[key+'_err']=fit_rr[key+'_err']
         
         
-        new_row['piE'] = piE_rr
-        new_row['piE_err'] = err_piE_rr
+        new_data_rr['piE'] = piE_rr
+        new_data_rr['piE_err'] = err_piE_rr
         #new_row['piE_err_MC'] = err_piE_rr_MC
-        new_row['chichi'] = chi_rr
-        new_row['dof'] = dof_rr
+        new_data_rr['chichi'] = chi_rr
+        new_data_rr['dof'] = dof_rr
         
         #new_row['mass_thetaE'] = Event.fit_mass_rr1()
         #new_row['mass_mu'] = Event.fit_mass_rr2()
@@ -99,25 +110,25 @@ for SET in tqdm(range(1,5)):
         #new_row['mass_err_thetaE'] = dict_mass['sigma_m_thetaE_rr']
         #new_row['mass_err_mu'] = dict_mass['sigma_m_mu_rr']
         #new_row['mass_err_thetaS'] = dict_mass['sigma_m_thetaS_rr']
-        
-        fit_rr_df = pd.concat([fit_rr_df, pd.DataFrame([new_row])], ignore_index=True)
+        new_data_rr.to_csv(filename, mode="a", header=False, index=False)
+        #fit_rr_df = pd.concat([fit_rr_df, pd.DataFrame([new_row])], ignore_index=True)
         
         
         # df de Roman 
-        
-        new_row = {}
-        new_row['Source']=nevent
-        new_row['Set']=nset
+        new_data_roman = pd.DataFrame(columns=cols_fit)
+        #new_row = {}
+        new_data_roman['Source']=nevent
+        new_data_roman['Set']=nset
         for key in Event.labels_params():
-            new_row[key]=fit_roman[key]
-            new_row[key+'_err']=fit_roman[key+'_err']
+            new_data_roman[key]=fit_roman[key]
+            new_data_roman[key+'_err']=fit_roman[key+'_err']
         
         
-        new_row['piE'] = piE_roman
-        new_row['piE_err'] = err_piE_roman
+        new_data_roman['piE'] = piE_roman
+        new_data_roman['piE_err'] = err_piE_roman
         #new_row['piE_err_MC'] = err_piE_roman_MC
-        new_row['chichi'] = chi_roman
-        new_row['dof'] = dof_roman
+        new_data_roman['chichi'] = chi_roman
+        new_data_roman['dof'] = dof_roman
         
         #new_row['mass_thetaE'] = Event.fit_mass_roman1()
         #new_row['mass_mu'] = Event.fit_mass_roman2()
@@ -129,10 +140,8 @@ for SET in tqdm(range(1,5)):
         #new_row['mass_err_mu'] = dict_mass['sigma_m_mu_roman']
         #new_row['mass_err_thetaS'] = dict_mass['sigma_m_thetaS_roman']
         
-        fit_roman_df = pd.concat([fit_roman_df, pd.DataFrame([new_row])], ignore_index=True)
+        new_data_roman.to_csv(filename, mode="a", header=False, index=False)
 
 
 
-true_df.to_csv(save_results+'true.csv', index=False)
-fit_roman_df.to_csv(save_results+'fit_roman.csv', index=False)
-fit_rr_df.to_csv(save_results+'fit_rr.csv', index=False)
+
